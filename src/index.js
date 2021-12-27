0077c897e7d4f0638c6c70f22c52439e9f217e59
Edilson Matola <edilsonmatola13@gmail.com>
2021-12-27 13:15:49 +0200

    Removing unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Hello from "./Hello";
+import Card from "./Card";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
+import { robots } from "./robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello sayHi = {'Hi there!'} />
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
