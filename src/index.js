645614e6b4056007bf3f6cbd8f75afc60ad51565
Edilson Matola <edilsonmatola13@gmail.com>
2021-12-27 14:21:00 +0200

    Optimizing the code 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import CardList from "./CardList";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import { robots } from "./robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
